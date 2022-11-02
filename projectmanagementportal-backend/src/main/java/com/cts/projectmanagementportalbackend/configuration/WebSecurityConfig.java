7a9c9cd72979ae7a40c445b8f4e58ac950271aff
unknown <cogjava1810@iiht.tech>
2022-11-02 19:58:38 +0530

    PMP-backend-security-endpoints  02-11-2022 2000

--- projectmanagementportal-backend/src/main/java/com/cts/projectmanagementportalbackend/configuration/WebSecurityConfig.java
+++ projectmanagementportal-backend/src/main/java/com/cts/projectmanagementportalbackend/configuration/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.cts.projectmanagementportalbackend.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,6 +32,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
+		
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET).hasAnyRole("ADMIN","MEMBER")
+			.antMatchers(HttpMethod.POST,"/api/user/userSignUp").hasAnyRole("ADMIN","MEMBER")
+			.antMatchers(HttpMethod.POST,".api/project/**").hasAnyRole("ADMIN")
+			.antMatchers(HttpMethod.PUT,".api/project/**").hasAnyRole("ADMIN")
+			.antMatchers(HttpMethod.POST,"/api/story/**").hasAnyRole("ADMIN")
+			.antMatchers(HttpMethod.PUT,"/api/story/**").hasAnyRole("ADMIN","MEMBER")
+			.antMatchers(HttpMethod.DELETE).hasAnyRole("ADMIN");
+		
+		http.cors().disable();
+		http.csrf().disable();
 		super.configure(http);
 	}
 	
@@ -39,6 +52,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 	
+	/*
+	 * This is not recommended -- please use permitAll via HttpSecurity#authorizeHttpRequests instead.
+	 */
 	@Override
 	public void configure(WebSecurity web) throws Exception{
 		web
